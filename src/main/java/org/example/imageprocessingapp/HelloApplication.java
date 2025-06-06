bd21dfb226efe6c159e462ed319815342f4952c4
M1K45 <272570@student.pwr.edu.pl>
2025-06-06 12:59:34 +0200

    created dropdown with choice validation

--- src/main/java/org/example/imageprocessingapp/HelloApplication.java
+++ src/main/java/org/example/imageprocessingapp/HelloApplication.java
@@ -3,7 +3,10 @@ package org.example.imageprocessingapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
 
@@ -11,12 +14,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+
+
+        stage.setTitle("Image processing app");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/org/example/imageprocessingapp/HelloController.java
+++ src/main/java/org/example/imageprocessingapp/HelloController.java
@@ -1,14 +1,41 @@
 package org.example.imageprocessingapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label errorLabel;
+    @FXML
+    private ComboBox dropdown;
+
+
+    @FXML
+    protected void initialize() {
+        errorLabel.setVisible(false);
+        errorLabel.setText("Nie wybrano operacji do wykonania");
+        errorLabel.setVisible(false);
+
+        dropdown.getItems().add("Choice 1");
+        dropdown.getItems().add("Choice 2");
+        dropdown.getItems().add("Choice 3");
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        String value = (String) dropdown.getValue();
+        if (value != null) {
+            errorLabel.setVisible(false);
+
+        }
+        else {
+            errorLabel.setVisible(true);
+        }
     }
 }
